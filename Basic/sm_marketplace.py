b2cc5fdfa8a1f0191d01b100de779167ce939684
Victoria <43069357+vtorygit@users.noreply.github.com>
2019-09-21 19:09:09 +0300

    Update sm_marketplace.py

--- Basic/sm_marketplace.py
+++ Basic/sm_marketplace.py
@@ -1,15 +1,3 @@
-from urllib.parse import urlparse
-
-from newsfeeds.spiders.templates.sitemap_template import ModSitemapSpider
-from newsfeeds.items import ContentItems
-from newsfeeds.item_functions import (process_item,
-                                      process_singular_item,
-                                      process_date_item,
-                                      process_array_item,
-                                      process_plural_texts,
-                                      process_external_links,
-                                      process_article_text)
-
 
 HEADER_XPATH = ['//h1/text()']
 AUTHOR_XPATH = ['//span[@class="c-story-head__body-meta-author bold"]/text()']
@@ -21,22 +9,4 @@ INTERLINKS = ['//div[@class="original-content"]/p//@href']
 #2004-01-17T21:13:37+00:00  
 DATE_FORMAT_STRING = '%Y-%m-%d'
 
-class marketPlaceSpider(ModSitemapSpider):
-    name = 'sm_marketplace'
-    allowed_domains = ["marketplace.org"]
-    sitemap_urls = ['https://www.marketplace.org/sitemap-index-1.xml']
-
-    def parse(self, response):
-        items = []
-        item = ContentItems()
-        item['title'] = process_singular_item(self, response, HEADER_XPATH, single=True)
-        item['resource'] = urlparse(response.url).hostname
-        item['author'] = process_array_item(self, response, AUTHOR_XPATH, single=False)
-        item['pubdate'] = process_date_item(self, response, PUBDATE_XPATH, DATE_FORMAT_STRING, single=True)
-        item['tags'] = process_plural_texts(self, response, TAGS_XPATH, single=False)
-        item['category'] = process_plural_texts(self, response, CATEGORY_XPATH, single=False)
-        item['article_text'] = process_article_text(self, response, TEXT_XPATH)
-        item['external_links'] = process_external_links(self, response, INTERLINKS, single=False)
-        item['link'] = response.url
-        items.append(item)
-        return items
+# marketplace.org
